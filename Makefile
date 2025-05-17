3003b0f84d5b921cda4c1781af5a6faa01f0d4cc
Carlos Alberto Garcia Rosales <carloga2@c2r6s4.42malaga.com>
2025-05-17 18:29:13 +0200

    mvp

--- Makefile
+++ Makefile
@@ -6,7 +6,46 @@
 #    By: carloga2 <carloga2@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/05/17 16:33:12 by carloga2          #+#    #+#              #
-#    Updated: 2025/05/17 16:33:14 by carloga2         ###   ########.fr        #
+#    Updated: 2025/05/17 18:25:34 by carloga2         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
+NAME = libftprintf.a
+
+CC = cc
+CFLAGS = -Wall -Wextra -Werror
+AR = ar rcs
+RM = rm -f
+
+# Source files
+SRCS = ft_printf.c
+
+# Object files
+OBJS = $(SRCS:.c=.o)
+
+# Header files
+HEADER = printf.h
+
+# Targets
+all: $(NAME)
+
+$(NAME): $(OBJS)
+	@$(AR) $(NAME) $(OBJS)
+
+# Compile source files
+%.o: %.c $(HEADER)
+	@$(CC) $(CFLAGS) -c $< -o $@
+
+# Clean object files
+clean:
+	@$(RM) $(OBJS)
+
+# Clean object files and library
+fclean: clean
+	@$(RM) $(NAME)
+
+# Rebuild everything
+re: fclean all
+
+# Non-file targets
+.PHONY: all clean fclean re test
---
+++ ft_printf.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: carloga2 <carloga2@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/17 18:22:39 by carloga2          #+#    #+#             */
+/*   Updated: 2025/05/17 18:28:49 by carloga2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int ft_printf(char const *format, ...)
+{
+    // Implementation of the ft_printf function
+    // This function will handle formatted output similar to printf
+    // It will take a format string and a variable number of arguments
+    // The implementation details will depend on the specific requirements
+    // and the types of format specifiers you want to support.
+
+    (void)format; // To avoid unused parameter warning
+    
+    return 0; // Placeholder return value
+}
